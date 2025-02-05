a5fc7e2c0f8f56f4369cbe498e202a8296f04f7c
anuvinddubey1902 <155791937+anuvinddubey1902@users.noreply.github.com>
2025-02-05 12:30:07 +0530

    Update lungcancer.py

--- lungcancer.py
+++ lungcancer.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import seaborn as sns
